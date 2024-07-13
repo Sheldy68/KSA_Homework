490ae18345eca4dc8d402166b627d3c4a04b9200
KorotaevaSA <sheldy68@gmail.com>
2024-07-13 22:32:08 +0300

    Commit for Module5 home work fix

--- src/Module5_KorotaevaSA/Middle/Task2.java
+++ src/Module5_KorotaevaSA/Middle/Task2.java
@@ -9,31 +9,29 @@ public class Task2 {
         N может принимать значение от 1 до 100. Пример:
          */
         Scanner scan = new Scanner(System.in);
-        System.out.println("Укажите число элементов в массиве:");
-        int n = scan.nextInt();
+        System.out.println("Укажите число элементов в массиве от 1 до 100:");
+        int numberOfElements = scan.nextInt();
         System.out.println("Введите эелементы массива по одному:");
-        float[] array = new float[n];
+        float[] array = new float[numberOfElements];
         //запись вводимых значений с консоли в массив
         for (int i = 0; i < array.length; i++) {
             array[i] = scan.nextFloat();
         }
         //цикл по массиву
         float maxABS = array[0];
-        float max = array[0];
+        float max = 0;
         for (int i = 0; i < array.length; i++) {
             maxABS = Math.max(maxABS, Math.abs(array[i]));
-            max = array[i];
+            if (Math.abs(array[i]) == maxABS) {
+                max = array[i]; //число с учетом знака
+            }
         }
         System.out.println("Максимальное по модулю значение у элемента: " + max);
         /* результат:
-            Укажите число элементов в массиве:
+            Укажите число элементов в массиве от 1 до 100:
             5
             Введите эелементы массива по одному:
-            -2,5
-            0
-            1
-            3,6
-            -5,1
+            -2,5 0 1 -5,1 4
             Максимальное по модулю значение у элемента: -5.1
          */
     }
